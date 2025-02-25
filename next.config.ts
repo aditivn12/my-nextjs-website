62bc02372c6eeafa7a35af70d53ccfbc2bf0f77b
aditivn12 <aditivn@ad.unc.edu>
2025-02-24 19:05:49 -0500

    undo router

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-const nextConfig = {
-  experimental: { appDir: true },
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
